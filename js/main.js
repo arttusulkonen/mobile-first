acae91316748bee04f18a4ed5835a3d876ff5374
Arttu Sulkonen <arttusulkonen@Arttus-MacBook-Pro.local>
2023-08-13 18:17:57 +0300

    add slick slider. create Hero section..

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(function(){
-
+  $('.responsive').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          
+        }
+      }
+    ]
+  });
 });
